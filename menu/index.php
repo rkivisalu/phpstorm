2fdc1e6a4438a371084332c5ed0a7a76139db747
rkivi <raul-markus.kivisalu@khk.ee>
2019-02-14 13:06:08 +0200

    m

--- menu/index.php
+++ menu/index.php
@@ -5,15 +5,62 @@ require_once(BASE_DIR.'conf.php');
 // vajalikud vaade objektid
 $mainTmpl = new Template('main');
 $contentTmpl = new Template('content');
-$cardTmpl = new Template('card');
 
-$mainTmpl->set('title', 'Menu Application');
+$categories = array(
+    array(
+        'name' => 'praed',
+        'icon' => 'fa-utensils',
+        'data' => array(
+            array(
+                'name' => 'Sealihapada ploomide ja aprikoosiga',
+                'description' => 'sealihapada, lisand, salat, leib',
+                'price' => 2.65,
+                'discount' => 2.25
+            ),
+            array(
+                'name' => 'Praetud kanakints',
+                'description' => 'praetud kana, lisand, salat, leib',
+                'price' => 2.50,
+                'discount' => 2.13
+            )
+        )
+    ),
+    array(
+        'name' => 'supid',
+        'icon' => 'fa-utensil-spoon',
+        'data' => array(
+            array(
+                'name' => 'Rassolnik',
+                'description' => 'supp, hapukoor, leib',
+                'price' => 1.10,
+                'discount' => 0.94
+            )
+        )
+    )
+);
+
+$mainTmpl->set('title', 'Menu App');
+foreach ($categories as $category){
+    $cardTmpl = new Template('card');
+    $cardHeaderTmpl = new Template('header');
+    $cardDataTmpl = new Template('data');
+    $cardHeaderTmpl->set('category', $category['name']);
+    $cardHeaderTmpl->set('icon', $category['icon']);
+    $cardTmpl->set('card_header', $cardHeaderTmpl->parse());
 
 
-$contentTmpl->add('cards', $cardTmpl->parse());
-$contentTmpl->add('cards', $cardTmpl->parse());
-$contentTmpl->add('cards', $cardTmpl->parse());
-$contentTmpl->add('cards', $cardTmpl->parse());
+    $cardDataTmpl->set('category', $category['name']);
+
+    $listTmpl = new Template('list');
+    foreach ($category['data'] as $dish){
+        foreach ($dish as $name=>$value){
+            $listTmpl->set($name, $value);
+        }
+        $cardDataTmpl->add('dish_list', $listTmpl->parse());
+    }
+    $cardTmpl->set('card_data', $cardDataTmpl->parse());
+    $contentTmpl->add('cards', $cardTmpl->parse());
+}
 
 $mainTmpl->set('content', $contentTmpl->parse());
 
